615653ecaa47de6c8051179f09418355c611bf22
unknown <zach.jilani1@gmail.com>
2023-07-22 23:55:35 -0600

    fixed indentation

--- cpp/searching/binarySearch.cpp
+++ cpp/searching/binarySearch.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int binarySearch(int arr[], int left, int right, int target)
 {
-    int mid;
-    if (right >= left)
+  int mid;
+  if (right >= left)
+  {
+    mid = floor(left + (right - left) / 2);
+    if (arr[mid] == target)
     {
-        mid = floor(left + (right - left) / 2);
-        if (arr[mid] == target)
-        {
-            return mid;
-        }
-        if (arr[mid] > target)
-        {
-            return binarySearch(arr, left, mid - 1, target);
-        }
-        else
-        {
-            return binarySearch(arr, mid + 1, right, target);
-        }
+      return mid;
+    }
+    if (arr[mid] > target)
+    {
+      return binarySearch(arr, left, mid - 1, target);
     }
     else
     {
-        return -1;
+      return binarySearch(arr, mid + 1, right, target);
     }
+  }
+  else
+  {
+    return -1;
+  }
 }
 
 int main()
 {
-    int l[10] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
-    int size = 10;
-    int target = 38;
-    int res = binarySearch(l, 0, 10, target);
-    cout << res << endl;
+  int l[10] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+  int size = 10;
+  int target = 38;
+  int res = binarySearch(l, 0, 10, target);
+  cout << res << endl;
 }

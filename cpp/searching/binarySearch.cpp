a03b150ff04566b0f3dafc4d19d4d34c15204153
unknown <zach.jilani1@gmail.com>
2023-07-22 23:54:01 -0600

    added cpp versions of algorithms

---
+++ cpp/searching/binarySearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int target)
+{
+    int mid;
+    if (right >= left)
+    {
+        mid = floor(left + (right - left) / 2);
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (arr[mid] > target)
+        {
+            return binarySearch(arr, left, mid - 1, target);
+        }
+        else
+        {
+            return binarySearch(arr, mid + 1, right, target);
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int l[10] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = 10;
+    int target = 38;
+    int res = binarySearch(l, 0, 10, target);
+    cout << res << endl;
+}

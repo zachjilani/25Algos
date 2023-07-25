248543d124d11972d0d426650dbeebfda727b6ff
unknown <zach.jilani1@gmail.com>
2023-07-24 22:00:59 -0600

    finished bellmen algorithm.

--- graphs/bellman.py
+++ graphs/bellman.py
@@ -5,8 +5,10 @@ maxint = 9999999999
 class Graph():
   def __init__(self, vertices):
     self.V = vertices
-    self.graph = [[0 for column in range(vertices)]
-                  for row in range(vertices)]
+    self.graph = []
+
+  def add_edges(self, u, v, w):
+    self.graph.append([u, v, w])
 
   def print_res(self, dist):
     print("Vertex \tDistance from Source")
@@ -16,5 +18,14 @@ class Graph():
   def bellman(self, src):
     dist = [maxint] * self.V
     dist[src] = 0
+    for _ in range(self.V - 1):
+      for u, v, w in self.graph:
+        if dist[u] != maxint and dist[u] + w < dist[v]:
+          dist[v] = dist[u] + w
+
+    for u, v, w in self.graph:
+      if dist[u] != maxint and dist[u] + w < dist[v]:
+        dist[v] = dist[u] + w
+    self.print_res(dist)
 
     return
